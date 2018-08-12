2ef1c047a7293a860396dd2ceccb00913a409693
Reda Baydoun <reda.baydoun@gmail.com>
2018-08-12 10:04:16 -0400

    Add option

--- module_3/src/main.rs
+++ module_3/src/main.rs
@@ -2,8 +2,9 @@
 
 fn main()
 {
-  structures();
-  enums();
+  //structures();
+  //enums();
+  option();
 }
 
 // Enumerations
@@ -27,13 +28,37 @@ fn enums()
     Color::Blue => println!("b"),
     Color::RgbColor(0, 0, 0) => println!("Black"),
     Color::RgbColor(r, g, b) => println!("RGB ({}. {}. {})", r, g, b),
-    Color::CmykColor{cyan:_, magenta:_, yellow:_, black:_} => println!("Cmyk")
+    Color::CmykColor{cyan:_, magenta:_, yellow:_, black:_} => println!("Cmyk"),
     // Color::CmykColor{cyan:_, magenta:_, yellow:_, black:_} => println!("Cmyk ({}, {}, {}, {})",
     //   Color::CmykColor.cyan,
     //   Color::CmykColor.magenta,
     //   Color::CmykColor.yellow,
     //   Color::CmykColor.black)
+    //_ => println!("Some other colour")
   }
+}
+
+fn option()
+{
+  // Option<T>
+
+  let x = 3.0;
+  let y = 2.0;
+
+  // Some (z) None ()
+  let result:Option<f64> =
+    if y != 0.0 { Some(x / y) } else { None };
+
+  println!("Result = {:?}", result);
+
+  match result
+  {
+    Some(z) => println!("{} / {} = {}", x, y, z),
+    None => println!("Can't devide {} by {}", x, y)
+  }
+
+  // if let / while let
+  if let Some(z) = result { println!("z = {}", z); }
 }
 
 // Structures
